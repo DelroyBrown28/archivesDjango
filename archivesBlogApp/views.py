0e13fb9e4b324dc08198eb0af8af1177c0b5c2be
Delroy Brown <Delroy_jb@hotmail.co.uk>
2020-09-13 21:25:12 +0100

    added templates, home page and about page

--- archivesBlogApp/views.py
+++ archivesBlogApp/views.py
@@ -1,10 +1,28 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+posts = [
+    {
+        'author': 'Delroy Brown',
+        'title': 'Blog Post 1',
+        'content': 'First post content',
+        'date_posted': 'September 12th, 2020'
+    },
+    {
+        'author': 'Rihanna Fenty',
+        'title': 'Blog Post 2',
+        'content': 'Second post content',
+        'date_posted': 'September 13th, 2020'
+    }
+]
+
 
 def home(request):
-    return HttpResponse("<h1>Blog Home</h1>")
+    context = {
+        'posts': posts
+    }
+    return render(request, 'blog/home.html', context)
 
 
 def about(request):
-    return HttpResponse("<h1>Blog About</h1>")
+    return render(request, 'blog/about.html', {'title': 'About'})
