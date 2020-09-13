e121e7356c0080775143fe489c777f57c333ccf2
Delroy Brown <Delroy_jb@hotmail.co.uk>
2020-09-13 23:16:21 +0100

    added post through django.db

--- archivesBlogApp/views.py
+++ archivesBlogApp/views.py
@@ -1,25 +1,11 @@
 from django.shortcuts import render
+from .models import Post
 from django.http import HttpResponse
-
-posts = [
-    {
-        'author': 'Delroy Brown',
-        'title': 'Blog Post 1',
-        'content': 'First post content',
-        'date_posted': 'September 12th, 2020'
-    },
-    {
-        'author': 'Rihanna Fenty',
-        'title': 'Blog Post 2',
-        'content': 'Second post content',
-        'date_posted': 'September 13th, 2020'
-    }
-]
 
 
 def home(request):
     context = {
-        'posts': posts
+        'posts': Post.objects.all()
     }
     return render(request, 'blog/home.html', context)
 
