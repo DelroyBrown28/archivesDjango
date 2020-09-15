38ffd97c45bf49464038faa8ed649bb499776fe6
Delroy Brown <Delroy_jb@hotmail.co.uk>
2020-09-15 16:27:08 +0100

    able to post to blog, edit and delete posts

--- archivesBlogApp/views.py
+++ archivesBlogApp/views.py
@@ -1,4 +1,10 @@
 from django.shortcuts import render
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
+from django.views.generic import (ListView,
+                                  DetailView,
+                                  CreateView,
+                                  UpdateView,
+                                  DeleteView)
 from .models import Post
 from django.http import HttpResponse
 
@@ -8,6 +14,63 @@ def home(request):
         'posts': Post.objects.all()
     }
     return render(request, 'blog/home.html', context)
+
+
+# Blog posts
+class PostListView(ListView):
+    model = Post
+    template_name = 'blog/home.html'
+    context_object_name = 'posts'
+    # Change to '-[date_posted]' to reverse post order
+    ordering = ['-date_posted']
+
+
+class PostDetailView(DetailView):
+    model = Post
+    template_name = 'blog/post_detail.html'
+
+
+class PostCreateView(LoginRequiredMixin, CreateView):
+    model = Post
+    fields = ['title', 'content']
+    template_name = 'blog/post_form.html'
+
+    # Sets author to current logged in user
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+
+
+class PostUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
+    model = Post
+    fields = ['title', 'content']
+    template_name = 'blog/post_form.html'
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+
+        # gets current logged in user and checks if its equal to
+        # the owner of the post thats getting updated
+    def test_func(self):
+        post = self.get_object()
+        if self.request.user == post.author:
+            return True
+        return False
+
+
+class PostDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
+    model = Post
+    success_url = '/'
+    template_name = 'blog/post_confirm_delete.html'
+
+    def test_func(self):
+        post = self.get_object()
+        # gets current logged in user and checks if its equal to
+        # the owner of the post thats getting updated
+        if self.request.user == post.author:
+            return True
+        return False
 
 
 def about(request):
