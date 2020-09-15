a9295e1cae0b45f99f1dde9c7adb5da4b4e1e438
Delroy Brown <Delroy_jb@hotmail.co.uk>
2020-09-15 22:52:44 +0100

    added pagination for main blog page

--- archivesBlogApp/urls.py
+++ archivesBlogApp/urls.py
@@ -3,11 +3,13 @@ from .views import (PostListView,
                     PostDetailView,
                     PostCreateView,
                     PostUpdateView,
-                    PostDeleteView)
+                    PostDeleteView,
+                    UserPostListView)
 from . import views
 
 urlpatterns = [
     path('', PostListView.as_view(), name='blog-home'),
+    path('user/<str:username>', UserPostListView.as_view(), name='user-posts'),
     # shows post based on primary key <pk>
     path('post/<int:pk>/', PostDetailView.as_view(), name='post-detail'),
     path('post/new/', PostCreateView.as_view(), name='post-create'),
--- archivesBlogApp/views.py
+++ archivesBlogApp/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
+from django.contrib.auth.models import User
 from django.views.generic import (ListView,
                                   DetailView,
                                   CreateView,
@@ -21,8 +22,20 @@ class PostListView(ListView):
     model = Post
     template_name = 'blog/home.html'
     context_object_name = 'posts'
-    # Change to '-[date_posted]' to reverse post order
+    # Change to '[date_posted]' to reverse post order
     ordering = ['-date_posted']
+    paginate_by = 5
+
+
+class UserPostListView(ListView):
+    model = Post
+    template_name = 'blog/user_posts.html'
+    context_object_name = 'posts'
+    paginate_by = 5
+
+    def get_queryset(self):
+        user = get_object_or_404(User, username=self.kwargs.get('username'))
+        return Post.objects.filter(author=user).order_by('-date_posted')
 
 
 class PostDetailView(DetailView):
@@ -51,7 +64,7 @@ class PostUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
         return super().form_valid(form)
 
         # gets current logged in user and checks if its equal to
-        # the owner of the post thats getting updated
+        # the owner of the post thats getting UPDATED
     def test_func(self):
         post = self.get_object()
         if self.request.user == post.author:
@@ -67,7 +80,7 @@ class PostDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
     def test_func(self):
         post = self.get_object()
         # gets current logged in user and checks if its equal to
-        # the owner of the post thats getting updated
+        # the owner of the post thats getting DELETED
         if self.request.user == post.author:
             return True
         return False
