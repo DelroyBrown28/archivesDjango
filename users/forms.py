3d9f08dbf91e16bdd7a61dee58e03bceae61887e
Delroy Brown <Delroy_jb@hotmail.co.uk>
2020-09-15 13:55:18 +0100

    can now update users details including profile image, profile image auto resizes when uploaded

--- users/forms.py
+++ users/forms.py
@@ -1,13 +1,28 @@
 from django import forms
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
-
+from .models import Profile
 
 
 class UserRegisterForm(UserCreationForm):
     email = forms.EmailField()
     # keeps configurations in once place
+
     class Meta:
         # specifies the form to be interacted with
         model = User
         fields = ['username', 'email', 'password1', 'password2']
+
+
+class UserUpdateForm(forms.ModelForm):
+    email = forms.EmailField()
+
+    class Meta:
+        model = User
+        fields = ['username', 'email']
+
+
+class ProfileUpdateForm(forms.ModelForm):
+    class Meta:
+        model = Profile
+        fields = ['image']
