0774847be36d8395533acca97347ab90e7a6f9fa
Delroy Brown <Delroy_jb@hotmail.co.uk>
2020-09-19 01:23:16 +0100

    cleaned up register page, could still use some work

---
+++ archivesBlogApp/static/blog/script.js
@@ -0,0 +1,51 @@
+$(document).ready(function () {
+
+    // Goodbye alert message!
+
+    $('.alert-success').click(function () {
+        $(this).fadeOut();
+    })
+
+    // onblur/focus events for register page
+    let registerUsernameInput = document.getElementById("id_username");
+    let registerPasswordInput = document.getElementById("id_password1");
+    let registerPasswordConfirmInput = document.getElementById("id_password2");
+
+    registerUsernameInput.onblur = usernameBlur;
+    registerUsernameInput.onfocus = usernameFocus;
+
+    registerPasswordInput.onblur = passwordBlur;
+    registerPasswordInput.onfocus = passwordFocus;
+
+    registerPasswordConfirmInput.onblur = passwordConfirmBlur;
+    registerPasswordConfirmInput.onfocus = passwordConfirmFocus;
+
+    function usernameFocus() {
+        $('#hint_id_username').addClass('show_hint')
+    }
+
+    function usernameBlur() {
+        $('#hint_id_username').removeClass('show_hint')
+    }
+
+    function passwordFocus() {
+        $('#hint_id_password1').addClass('show_hint')
+    }
+
+    function passwordBlur() {
+        $('#hint_id_password1').removeClass('show_hint')
+    }
+
+    function passwordConfirmFocus() {
+        $('#hint_id_password2').addClass('show_hint')
+    }
+
+    function passwordConfirmBlur() {
+        $('#hint_id_password2').removeClass('show_hint')
+    }
+
+
+
+
+
+})
